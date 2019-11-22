9dd9f5eb7fb038697584e71738151d5508fec725
Chris Grimmett <chris@grimtech.net>
2019-11-22 10:12:33 -0800

    add jepurdee client as npm module instead of submodule

--- src/spaServer.js
+++ src/spaServer.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 const main = (app, express) => {
-  app.use(express.static('jepurdee/dist'));
+  app.use(express.static('node_modules/jepurdee/dist'));
 
   app.all('*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'jepurdee/dist/index.html'));
